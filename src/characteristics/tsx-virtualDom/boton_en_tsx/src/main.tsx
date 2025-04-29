cff367bb45df728cc797b6f95ec0fcfa995df16c
Javier Gomez Alayon <alu0101562445@ull.edu.es>
2025-04-29 18:24:43 +0000

    fix: arreglando problema con uno de los ejemplos del botón

--- src/characteristics/tsx-virtualDom/boton_en_tsx/src/main.tsx
+++ src/characteristics/tsx-virtualDom/boton_en_tsx/src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { createElement } from 'react'
-import React from 'react'
 
-const button2 = React.createElement('button', { onClick: () => alert('Hola mundo') }, 'Pulsame')
-
-const button = createElement('button', { onClick: () => alert('Hola mundo') }, 'Pulsame')
-
-createRoot(document.getElementById('root')!).render(button)
-createRoot(document.getElementById('root2')!).render(button2)
+createRoot(document.getElementById('root')!).render(
+  <button onClick={() => alert('Hola mundo')}>Pulsame</button>
+)
